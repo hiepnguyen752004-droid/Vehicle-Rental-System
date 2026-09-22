38f36a78dee73a49ae5c9273c2ae0ad04d42d4b5
Austin Nguyen <hiepnguyen752004@gmail.com>
2026-09-21 21:22:15 -0500

    Refactor main.cpp to include InventoryManager

--- main.cpp
+++ main.cpp
@@ -1,13 +1,15 @@
 #include <iostream>
 
-#include "Entity/Vehicle.h"
-#include "Entity/Customer.h"
-#include "Entity/Reservation.h"
+#include "Vehicle.h"
+#include "Customer.h"
+#include "Reservation.h"
+#include "InventoryManager.h"
 
 using namespace std;
 
-int main() {
-    Vehicle vehicle(
+int main()
+{
+    Vehicle vehicle1(
         1,
         "Toyota",
         "Camry",
@@ -17,47 +19,97 @@ int main() {
         "Available"
     );
 
+    Vehicle vehicle2(
+        2,
+        "Honda",
+        "Civic",
+        2024,
+        "Sedan",
+        55.00,
+        "Available"
+    );
+
+    Vehicle vehicle3(
+        3,
+        "Ford",
+        "Explorer",
+        2025,
+        "SUV",
+        85.00,
+        "Available"
+    );
+
+    Vehicle vehicle4(
+        4,
+        "Chevrolet",
+        "Tahoe",
+        2024,
+        "SUV",
+        95.00,
+        "Unavailable"
+    );
+
+
+    InventoryManager inventory;
+
+    inventory.addVehicle(vehicle1);
+    inventory.addVehicle(vehicle2);
+    inventory.addVehicle(vehicle3);
+    inventory.addVehicle(vehicle4);
+
+
+    
+    inventory.viewVehicleBrands();
+
+
+    
+    string searchMake;
+
+    cout << endl;
+    cout << "Enter vehicle brand to search: ";
+    cin >> searchMake;
+
+    inventory.searchVehicleByMake(searchMake);
+
+
+   
     Customer customer(
-        
+        1,
         "Austin Nguyen",
-        "Asutin@email.com",
+        "Austin@email.com",
         "password123",
         "713-555-1234"
     );
 
+
+  
     Reservation reservation(
         1,
         customer.getCustomerId(),
-        vehicle.getVehicleId(),
+        vehicle1.getVehicleId(),
         "2026-09-20",
         "2026-09-25",
         300,
         "Confirmed"
     );
 
-    cout << "Vehicle: "
-        << vehicle.getYear() << " "
-        << vehicle.getMake() << " "
-        << vehicle.getModel()
-        << endl;
-
-    cout << "Category: "
-        << vehicle.getCategory()
-        << endl;
-
-    cout << "Daily rate: "
-        << vehicle.getDailyRate()
-        << endl;
 
     cout << endl;
+    cout << "CUSTOMER" << endl;
+    
 
-    cout << "customer: "
+    cout << "Customer: "
         << customer.getName()
         << endl;
 
-    cout << "email: "
+    cout << "Email: "
         << customer.getEmail()
         << endl;
+
+
+    cout << endl;
+    cout << "RESERVATION" << endl;
+    
 
     cout << "Reservation ID: "
         << reservation.getReservationId()
@@ -84,6 +136,7 @@ int main() {
     cout << "Status: "
         << reservation.getStatus()
         << endl;
+
 
     return 0;
 }
