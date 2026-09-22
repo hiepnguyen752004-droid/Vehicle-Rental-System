72f8920cd041ecedb7b0ab25978a68931e6be11d
Austin Nguyen <hiepnguyen752004@gmail.com>
2026-09-21 19:57:30 -0500

    Add InventoryManager class for vehicle management

---
+++ InventoryManager.h
@@ -0,0 +1,25 @@
+#ifndef INVENTORYMANAGER_H
+#define INVENTORYMANAGER_H
+
+#include <vector>
+#include <string>
+#include "Vehicle.h"
+
+using namespace std;
+
+class InventoryManager
+{
+private:
+    vector<Vehicle> vehicles;
+
+public:
+    void addVehicle(Vehicle vehicle);
+
+    void viewVehicleBrands() const;
+
+    void searchVehicleByMake(string make) const;
+
+    void removeVehicle(int id);
+};
+
+#endif
