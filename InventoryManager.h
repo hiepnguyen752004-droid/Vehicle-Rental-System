03dacbce9caeea047c03750117dd4e93a9481425
Austin Nguyen <hiepnguyen752004@gmail.com>
2026-09-24 11:29:04 -0500

    update InventoryManager.h

--- InventoryManager.h
+++ InventoryManager.h
@@ -4,7 +4,6 @@
 #include <vector>
 #include <string>
 #include "Vehicle.h"
-
 using namespace std;
 
 class InventoryManager
@@ -14,11 +13,8 @@ private:
 
 public:
     void addVehicle(Vehicle vehicle);
-
     void viewVehicleBrands() const;
-
     void searchVehicleByMake(string make) const;
-
     void removeVehicle(int id);
 };
 
